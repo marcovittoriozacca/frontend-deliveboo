731c095eeedc2d4d699b1ac2c39a8844cb8cd096
erosalpe <eros.alpegiani@gmail.com>
2024-04-24 12:31:53 +0200

    added bg image home page and diagonal first section

--- src/Pages/AppHome.vue
+++ src/Pages/AppHome.vue
@@ -11,7 +11,20 @@
 
 
 <template>
+    <div id="bg-first-section">
+        <div>
 
+        </div>
+    </div>
+    <section>
+        <div class="dritto-blu">
+            <div class="diagonale-blu"></div>
+        </div>
+        
+    </section>
+    
+
+    
 
   
 
@@ -26,8 +39,30 @@
 
 
 <style lang="scss" scoped>
-    
+    #bg-first-section{
+        background-image: url(../assets/img/bghome.png);
+        background-repeat: no-repeat;
+        filter: blur(4px);
+        -webkit-filter: blur(4px);
+        width: 100vw;
+        height: 500px;
+    }
 
+    .diagonale-blu {
+        width: 100%;
+        height: 500px;
+        background: #03071e;
+        z-index: 0;
+        transform: skewY(3deg);
+        transform-origin: top right;
+    }
+    .dritto-blu{
+        padding-top: 150px;
+        background: #03071e;
+        width: 100%;
+        height: 100px;
+    }
+       
 
 
 
--- src/router.js
+++ src/router.js
@@ -1,23 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import AppHome from './Pages/AppHome.vue';
+//import NotFound from './Pages/NotFound.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        // {
-        //     path: '/',
-        //     name: 'home',
-        //     component: AppHome,
-        // },
-        // {
-        //     path: '/',
-        //     name: '',
-        //     component: SingleRestaurant,
-        // },
-        // {
-        //     path: '/:patchMatch(.*)*',
-        //     name: 'not-found',
-        //     component: NotFound,
-        // },
+            {
+                path: '/',
+                name: 'home',
+                component: AppHome,
+            },
+            /*
+            {
+                path: '/',
+                name: '',
+                component: SingleRestaurant,
+            },
+            {
+                path: '/:patchMatch(.*)*',
+                name: 'not-found',
+                component: NotFound,
+            },
+            */
     ]
 })
 
