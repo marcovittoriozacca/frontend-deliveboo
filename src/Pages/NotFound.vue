d1fc409fabb6e11f8263286e4f89cb427511d7de
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-04-24 12:15:32 +0200

    update link

---
+++ src/Pages/NotFound.vue
@@ -0,0 +1,34 @@
+<script>
+    export default {
+        name: "NotFound",
+    }
+    
+</script>
+
+
+
+
+
+
+<template>
+
+
+  
+<h1>aaaaaaaaa</h1>
+
+</template>
+
+
+
+
+
+
+
+
+<style lang="scss" scoped>
+    
+
+
+
+
+</style>
