aa3561e52f5f2da5ec4db64ed3db5efd861a23a9
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-04-25 16:54:36 +0200

    add api call

--- src/Pages/SingleRestaurant.vue
+++ src/Pages/SingleRestaurant.vue
@@ -1,7 +1,44 @@
 <script>
-    export default {
-        name: "SingleRestaurant",
+import Plate from '../components/Plate.vue'
+import axios from 'axios';
+import {store} from '../Store';
+
+
+export default {
+        name: "AppHome",
+        components:{
+            Plate
+        },
+        props: {
+            activateType: String,
+        },
+        data(){
+            return{
+                store,
+                plate:{}
+                
+            }
+        },
+        methods:{
+            getPlate(){
+                axios.get('http://127.0.0.1:8000/api/restaurant/${this.$route.params.id}').then(res=>{
+                    this.plate=res.data.dishes
+                    console.log(this.plate)
+                })
+            },
+        },
+
+        mounted(){
+            this.getPlate();
+        },
     }
+    
+    
+
+
+
+        
+    
     
 </script>
 
