9c45f39a6831ca57ae6102b28dc088e110730626
erosalpe <eros.alpegiani@gmail.com>
2024-04-26 15:34:49 +0200

    feat: (Single Restaurant) now dishes card are divided by category and each category is hidden if has no dishes

--- src/Pages/SingleRestaurant.vue
+++ src/Pages/SingleRestaurant.vue
@@ -13,7 +13,10 @@ export default {
                 store,
                 plates: [],
                 restaurant: [],
-                
+                ArrayPrimi: [],
+                ArraySecondi: [],
+                ArrayDessert: [],
+                ArrayBevande: [],
             }
         },
         methods:{
@@ -21,8 +24,22 @@ export default {
                 axios.get(`http://127.0.0.1:8000/api/restaurant/${this.$route.params.slug}`).then((res)=> { 
                     this.plates = res.data.dishes
                     this.restaurant = res.data.restaurant
+                    this.dishSorterer();
                 } )
             },
+            dishSorterer(){
+                this.plates.forEach(plate => {
+                    if(plate.category_id == 1){
+                        this.ArrayPrimi.push(plate);
+                    } else if(plate.category_id == 2){
+                        this.ArraySecondi.push(plate);
+                    } else if(plate.category_id == 3){
+                        this.ArrayDessert.push(plate);
+                    } else if(plate.category_id == 4){
+                        this.ArrayBevande.push(plate);
+                    }
+                });
+            }
         },
         mounted(){
             this.getPlates();
@@ -31,26 +48,93 @@ export default {
 </script>
 
 <template>
-<div class="container">
-    <div class="text-center py-3">
-        <h1>{{ restaurant.activity_name }}</h1>
-        <p>{{ restaurant.address }}</p>
-        <p>{{ restaurant.piva }}</p>
+    <div class="bg-restaurant position-relative" :style="{ backgroundImage: 'url(http://127.0.0.1:8000/storage/' + restaurant.image + ')' }">
+        <div class=" position-absolute info-flex d-flex align-items-center">
+            <div class="text-center py-3 info-box rounded">
+                <h1>{{ restaurant.activity_name }}</h1>
+                <p>{{ restaurant.address }}</p>
+                <p>{{ restaurant.piva }}</p>
+            </div>
+            <img src="../assets/img/add-to-cart.png" alt="add-to-cart" class="add-to-cart">
+        </div>
     </div>
-    <div class="row">
-        <div class="col-12 col-md-6 col-lg-4 col-xl-3" v-for="(plate, index) in plates" :key="plate.id">
-            <div class="my-3">
+    <div class="container">
+        <div v-if="this.ArrayPrimi.length > 0">
+            <h2>Primi</h2>
+            <div class="d-flex justify-content-start flex-wrap gap-4">
                 <Plate
+                v-for="(plate, index) in this.ArrayPrimi" :key="plate.id" 
+                class="w-card"
+                :plate = plate
+                />
+            </div>
+        </div>
+        <div v-if="this.ArraySecondi.length > 0">
+            <h2>Secondi</h2>
+            <div class="d-flex justify-content-start flex-wrap gap-4">
+                <Plate
+                v-for="(plate, index) in this.ArraySecondi" :key="plate.id" 
+                class="w-card"
+                :plate = plate
+                />
+            </div>
+        </div>
+        <div v-if="this.ArrayDessert.length > 0">
+            <h2>Dessert</h2>
+            <div class="d-flex justify-content-start flex-wrap gap-4">
+                <Plate
+                v-for="(plate, index) in this.ArrayDessert" :key="plate.id" 
+                class="w-card"
+                :plate = plate
+                />
+            </div>
+        </div>
+        <div v-if="this.ArrayBevande.length > 0">
+            <h2>Bevande</h2>
+            <div class="d-flex justify-content-start flex-wrap gap-4">
+                <Plate
+                v-for="(plate, index) in this.ArrayBevande" :key="plate.id" 
+                class="w-card"
                 :plate = plate
                 />
             </div>
         </div>
     </div>
-</div>
 
 </template>
 
 
 <style lang="scss" scoped>
-    
+    .bg-restaurant{
+        width: 100%;
+        height: 400px;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        box-shadow: 0px 8px 25px 5px rgb(0, 0, 0, 0.5);
+        margin-bottom: 100px;
+    }
+
+    .info-box{
+        background-color: rgba(0, 0, 0, 0.7);
+        width: 30%;
+        color: white;
+    }
+
+    .info-flex{
+        top: 46%;
+        left: 5%;
+        width: 100%;
+        gap: 46rem;
+    }
+
+    .add-to-cart{
+        width: 25%;
+    }
+
+    .w-card{
+        --gap: 1.5rem;
+    	--columns: 3;
+    	flex-basis: calc((100% / var(--columns)) - var(--gap) + (var(--gap) / var(--columns)));
+    }
 </style>
