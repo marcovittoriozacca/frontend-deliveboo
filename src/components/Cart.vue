0a3864e87c21a1e7e080b6e18f7c351f3d8e9249
marcovittoriozacca <zaccamarco01@gmail.com>
2024-04-29 10:53:02 +0200

    feat: Cart Card style fixed

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -74,10 +74,17 @@ import CartCard from "./Header/CartCard.vue"
             <div class="d-grid">
                 <button class="my-3 btn btn-lg bg-warning ">Checkout</button>
             </div>
-            <!-- bottone remove all  -->
-            <div class="d-grid" @click="clearAll()">
-                <button class="my-3 btn btn-lg bg-danger ">Svuota Carrello</button>
-            </div>
+
+            <!-- Modale rimozione tutti i piatti -->
+            <button
+                type="button"
+                class="btn btn-danger btn-lg w-100"
+                data-bs-toggle="modal"
+                data-bs-target="#modalId"
+            >
+                Svuota Carrello
+            </button>
+
         </div>
         <div v-else>
             <h2 class="text-center text-white">Nessun piatto presente nel tuo carrello</h2>
@@ -85,8 +92,41 @@ import CartCard from "./Header/CartCard.vue"
   </div>
 </div>
 
+<div
+    class="modal fade"
+    id="modalId"
+    tabindex="-1"
+    data-bs-backdrop="static"
+    data-bs-keyboard="false"
+    
+    role="dialog"
+    aria-labelledby="modalTitleId"
+    aria-hidden="true"
+>
+    <div
+        class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-md"
+        role="document"
+    >
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title text-center" id="modalTitleId">
+                    Sei sicuro di voler rimuovere tutti i piatti dal tuo carrello?
+                </h5>
+                <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                ></button>
+            </div>
+            <div class="modal-footer">
+                <div class="text-center w-100" @click="clearAll()">
+                    <button class="my-3 btn btn-lg text-white bg-danger" data-bs-dismiss="modal">Svuota Carrello</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
-<style lang="scss" scoped>
-    
-</style>
+<style lang="scss" scoped></style>
