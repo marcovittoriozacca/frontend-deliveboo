42dd0652cbf94bc02d6f202ac9f537124a648cf1
EnzoPierucci <enzopierucci@gmail.com>
2024-04-29 16:07:57 +0200

    fix:font and size

--- src/components/HomeComponents/AnimationComp.vue
+++ src/components/HomeComponents/AnimationComp.vue
@@ -14,39 +14,34 @@ export default {
 </script>
 
 <template>
-
-<div class="animation-container">
-    <div class="row">
-      <div class="col-md-6">
-        <h1 class="animated-word" data-aos="fade-up" data-aos-delay="0">Cena con gli Amici?</h1>
-        <h1 class="animated-word" data-aos="fade-up" data-aos-delay="1500">Cosa mangio oggi?</h1>
-        <h1 class="animated-word" data-aos="fade-up" data-aos-delay="3000">Nessun Problema c'è</h1>
-      </div>
-      <div class="col-md-6 text-center">
-        <img src="/img/deliveboo-logo.png" alt="Immagine Deliveboo" class="animated-image" data-aos="fade-up" data-aos-delay="5000">
+    <div class="animation-container">
+      <div class="row">
+        <div class="col-md-6 gladiola-font">
+          <h1 class="animated-word gladiola-font" data-aos="fade-up" data-aos-delay="0">Cena con gli Amici?</h1>
+          <h1 class="animated-word gladiola-font" data-aos="fade-up" data-aos-delay="1500">Cosa mangio oggi?</h1>
+          <h1 class="animated-word gladiola-font" data-aos="fade-up" data-aos-delay="3000">Nessun Problema c'è</h1>
+        </div>
+        <div class="col-md-6 text-center">
+          <img src="/img/deliveboo-logo.png" alt="Immagine Deliveboo" class="animated-image" data-aos="fade-up" data-aos-delay="5000">
+        </div>
       </div>
     </div>
-  </div>
-  
   </template>
-
-
-<style lang="scss" scoped>
-
-.animation-container {
   
-
-  text-align: left; 
-}
-
-.animated-word {
-  color: white;
-  animation-duration: 0.5s;
-  animation-timing-function: ease-in-out; 
-  animation-delay: 0.1s; 
-
-  font-size: 24px; 
-  line-height: 1; 
-}
-
-</style>
+  <style lang="scss" scoped>
+  .animation-container {
+    padding-top: 50px; 
+    text-align: left; 
+  }
+  
+  .animated-word {
+    color: white;
+    font-size: 36px; 
+    line-height: 1.5; 
+  }
+  
+  .gladiola-font {
+    font-family: 'Gladiola', cursive; 
+  }
+  </style>
+  
